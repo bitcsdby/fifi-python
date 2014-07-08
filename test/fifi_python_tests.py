d7ef8db4dfa2677150970e8b38d8416296be115d
Jeppe Pihl <jpihl08@gmail.com>
2014-07-08 10:38:49 +0200

    added comments and cleaned up

--- test/fifi_python_tests.py
+++ test/fifi_python_tests.py
@@ -18,11 +18,20 @@ def read_json(file_path):
     with open(file_path) as data_file:
         return json.load(data_file)
 
+# Get absolute path of the test data file
 file_path = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), DATA_FILE_NAME)
 
+# Read test data
 test_data = read_json(file_path)
 
+# Store information about the fields:
+# key: Name,
+# implementations: Implementations for that specific field,
+# value_type: Based on python struct Format Characters, more info here:
+# https://docs.python.org/2/library/struct.html#byte-order-size-and-alignment
+# is_exact: Specifies whether the underlying type is exact, meaning that the
+# value type can contain exactly one element.
 fields = {
     'binary': {
         'implementations': [
@@ -77,10 +86,7 @@ class TestOperations(unittest.TestCase):
             for implementation in fields[field]['implementations']:
                 finite_field = implementation()
 
-                if not hasattr(finite_field, operation):
-                    return
-
-                for d in test_data[field].get(operation, []):
+                for d in test_data[field].get(operation) or []:
                     args = []
                     args.append(d['input1'])
                     if 'input2' in d:
@@ -114,32 +120,29 @@ class TestOperations(unittest.TestCase):
         self.do_test('packed_invert')
 
     def do_region_test(self, operation):
-        real_operation = "region_{}".format(operation)
         for field in fields:
+            # This unit test in not able to pack the elements if they are not
+            # exact.
             if not fields[field]['is_exact']:
                 continue
 
-            value_type = fields[field]['value_type']
+            # "<" forces little endian (and thereby a standard byte size).
+            # See "struct" documentation for more info.
+            value_type = "<{}".format(fields[field]['value_type'])
             for implementation in fields[field]['implementations']:
                 finite_field = implementation()
-
-                if not hasattr(finite_field, real_operation):
-                    return
 
                 input1 = bytes()
                 input2 = bytes()
                 expected = bytes()
 
-                for d in test_data[field].get(operation, []):
+                for d in test_data[field].get(operation) or []:
                     input1 += struct.pack(value_type, d['input1'])
                     if 'input2' in d:
                         input2 += struct.pack(value_type, d['input2'])
                     expected += struct.pack(value_type, d['result'])
 
-                input1 = bytes(input1)
-                input2 = bytes(input2)
-                expected = bytes(expected)
-
+                real_operation = "region_{}".format(operation)
                 result = getattr(finite_field, real_operation)(input1, input2)
                 self.assertEqual(result, expected)
 
