d24f7d42fcd8e9c09a9272b11383eb154347932e
Jeppe Pihl <jpihl08@gmail.com>
2014-07-22 14:29:08 +0200

    added license to python files

--- test/fifi_python_tests.py
+++ test/fifi_python_tests.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
+# Copyright Steinwurf ApS 2011-2013.
+# Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+# See accompanying file LICENSE.rst or
+# http://www.steinwurf.com/licensing
+
 import json
 import os
 import unittest
